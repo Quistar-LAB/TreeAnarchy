419d90292322e34be92022ea3d77572076543fc3
Simon Ueng <simon.ueng@outlook.com>
2021-05-27 10:43:33 +0800

    Info update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TreeAnarchy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TreeAnarchy Mod [AKA Unlimited Trees Mod]")]
+[assembly: AssemblyDescription("Unlimited Trees mod that supports Tree Snapping")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Quistar")]
 [assembly: AssemblyProduct("TreeAnarchy")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
