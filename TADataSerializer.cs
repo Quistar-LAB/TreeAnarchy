03c42a202ea7b7222add75578a6712e539e6e0e5
Simon Ueng <simon.ueng@outlook.com>
2021-05-28 06:13:31 +0800

    dynamic memory allocation / change class constructor so Dispose gets called only once

--- TADataSerializer.cs
+++ TADataSerializer.cs
@@ -26,9 +26,33 @@ namespace TreeAnarchy
             NONE = 0,
             PACKED = 1,
         }
+        [Flags]
+        public enum ErrorFlags : ushort
+        {
+            NONE = 0,
+            SUCCESS = 1,
+            FAILED = 2,
+            OLDFORMAT = 4,
+        }
 
         private bool disposed = false;
+        private readonly MemoryStream m_Stream = null;
         public MemoryStream m_tempBuf = null;
+
+        public TADataSerializer()
+        {
+            m_Stream = new MemoryStream();
+        }
+
+        protected TADataSerializer(byte[] data)
+        {
+            m_Stream = new MemoryStream(data);
+        }
+
+        public TADataSerializer(int size)
+        {
+            m_Stream = new MemoryStream(size);
+        }
 
         public abstract void FinalizeSave();
         public abstract void AfterDeserialize(Stream s);
@@ -59,7 +83,7 @@ namespace TreeAnarchy
         // a bit long because of all the different formats we have to consider
         // Keeping compatibility almost broke my brain reading the old codes,
         // but was a fun challenge
-        public void Deserialize(Stream s)
+        public ErrorFlags Deserialize(TreeInstance[] trees)
         {
             int treeCount = 0;
             int orgTreeCount = 0;
@@ -67,39 +91,37 @@ namespace TreeAnarchy
             int MaxLen;
             SaveFlags flags = 0;
 
-            TreeInstance[] trees = Singleton<TreeManager>.instance.m_trees.m_buffer;
-
-            Format version = (Format)ReadUShort(s);
+            Format version = (Format)ReadUShort();
             switch(version)
             {
                 case Format.Version1:
                     treeLimit = FormatVersion1TreeLimit;
                     break;
                 case Format.Version2:
-                    treeLimit = FixEndian(ReadInt32(s)); // Fixing ushort reorder
+                    treeLimit = FixEndian(ReadInt32()); // Fixing ushort reorder
                     break;
                 case Format.Version3:
-                    treeLimit = FixEndian(ReadInt32(s)); // Fixing ushort reorder
-                    treeCount = FixEndian(ReadInt32(s)); // Fixing ushort reorder
-                    ReadInt32(s); // Reserved for future use
-                    ReadInt32(s); // Reserved for future use
-                    flags = (SaveFlags)ReadUShort(s);
+                    treeLimit = FixEndian(ReadInt32()); // Fixing ushort reorder
+                    treeCount = FixEndian(ReadInt32()); // Fixing ushort reorder
+                    ReadInt32(); // Reserved for future use
+                    ReadInt32(); // Reserved for future use
+                    flags = (SaveFlags)ReadUShort();
                     break;
                 case Format.Version4:
-                    treeLimit = ReadInt32(s);
-                    treeCount = ReadInt32(s);
-                    orgTreeCount = ReadInt32(s); // used for reading posY between 0~262144 trees
-                    ReadInt32(s); // Reserved for future use
-                    flags = (SaveFlags)ReadUShort(s);
+                    treeLimit = ReadInt32();
+                    treeCount = ReadInt32();
+                    orgTreeCount = ReadInt32(); // used for reading posY between 0~262144 trees
+                    ReadInt32(); // Reserved for future use
+                    flags = (SaveFlags)ReadUShort();
                     break;
                 default:
-                    return;
+                    return ErrorFlags.FAILED;
             }
             /* Sanity check */
             if (treeLimit < 1 && treeCount < 0)
             {
                 Debug.Log(@"TreeAnarchy: Read Tree Capacity < 0. Invalid Data");
-                return;
+                return ErrorFlags.FAILED;
             }
             if(treeLimit > MaxTreeLimit) treeLimit = MaxTreeLimit; // Only load what MaxTreeLimit is limited to
             switch (version)
@@ -107,7 +129,6 @@ namespace TreeAnarchy
                 case Format.Version1:
                 case Format.Version2:
                 case Format.Version3:
-                    PreviousSaveWasOldFormat = true;
                     switch (flags & SaveFlags.PACKED)
                     {
                         case SaveFlags.NONE:
@@ -119,33 +140,33 @@ namespace TreeAnarchy
                             ReadData:
                             for (uint i = DefaultTreeLimit; i < MaxLen; i++)
                             {
-                                TreeInstance.Flags m_flags = (TreeInstance.Flags)ReadUShort(s);
+                                TreeInstance.Flags m_flags = (TreeInstance.Flags)ReadUShort();
                                 m_flags &= ~(TreeInstance.Flags.FireDamage | TreeInstance.Flags.Burning);
                                 trees[i].m_flags = (ushort)m_flags;
                                 if (trees[i].m_flags != 0)
                                 {
-                                    trees[i].m_infoIndex = ReadUShort(s); ;
-                                    trees[i].m_posX = ReadShort(s);
-                                    trees[i].m_posZ = ReadShort(s);
+                                    trees[i].m_infoIndex = ReadUShort(); 
+                                    trees[i].m_posX = ReadShort();
+                                    trees[i].m_posZ = ReadShort();
                                     trees[i].m_posY = 0; // old format doesn't save this
                                 }
                             }
                             break;
                     }
-                    break;
+                    return ErrorFlags.SUCCESS | ErrorFlags.OLDFORMAT;
                 case Format.Version4:
                     int treeAddCount = 0;
                     for (uint i = 1; i < DefaultTreeLimit; i++)
                     {
                         if(trees[i].m_flags != 0 && treeAddCount < orgTreeCount)
                         {
-                            trees[i].m_posY = ReadUShort(s);
+                            trees[i].m_posY = ReadUShort();
                             treeAddCount++;
                         }
                     }
                     for (uint i = DefaultTreeLimit; i < MaxTreeLimit; i++)
                     {
-                        TreeInstance.Flags m_flags = (TreeInstance.Flags)ReadUShort(s);
+                        TreeInstance.Flags m_flags = (TreeInstance.Flags)ReadUShort();
                         m_flags &= ~(TreeInstance.Flags.FireDamage | TreeInstance.Flags.Burning);
                         trees[i].m_flags = (ushort)m_flags;
                     }
@@ -153,10 +174,10 @@ namespace TreeAnarchy
                     {
                         if (trees[i].m_flags != 0)
                         {
-                            trees[i].m_infoIndex = ReadUShort(s);
-                            trees[i].m_posX = ReadShort(s);
-                            trees[i].m_posZ = ReadShort(s);
-                            trees[i].m_posY = ReadUShort(s);
+                            trees[i].m_infoIndex = ReadUShort();
+                            trees[i].m_posX = ReadShort();
+                            trees[i].m_posZ = ReadShort();
+                            trees[i].m_posY = ReadUShort();
                             treeCount++;
                         }
                         else
@@ -164,8 +185,9 @@ namespace TreeAnarchy
                             trees[i].m_posY = 0;
                         }
                     }
-                    break;
+                    return ErrorFlags.SUCCESS;
             }
+            return ErrorFlags.FAILED;
         }
 
         private ushort UpdatePosY(Vector3 position)
@@ -174,20 +196,19 @@ namespace TreeAnarchy
             return (ushort)Mathf.Clamp(Mathf.RoundToInt(position.y * 64f), 0, 65535);
         }
 
-        public void Serialize(Stream s)
+        public byte[] Serialize()
         {
             int orgTreeCount = 0;
             int extraTreeCount = 0;
             int treeLimit = MaxTreeLimit;
-            uint index;
             TreeInstance[] buffer = Singleton<TreeManager>.instance.m_trees.m_buffer;
 
-            WriteUShort(s, (ushort)Format.Version4); // 2 bytes for version
-            WriteInt32(s, treeLimit);                // 4 bytes for limit
-            WriteInt32(s, 0);         // Reserved for treeCount;    4 bytes
-            WriteInt32(s, 0);        // treeCount for posY in 0~262144 trees    4 bytes
-            WriteUInt32(s, 0);        // Reserved for future use    4 bytes
-            WriteUShort(s, 0);        // flags.. this is ignore in our version 2 bytes
+            WriteUShort((ushort)Format.Version4); // 2 bytes for version
+            WriteInt32(treeLimit);                // 4 bytes for limit
+            WriteInt32(0);         // Reserved for treeCount;    4 bytes
+            WriteInt32(0);        // treeCount for posY in 0~262144 trees    4 bytes
+            WriteUInt32(0);        // Reserved for future use    4 bytes
+            WriteUShort(0);        // flags.. this is ignore in our version 2 bytes
 
             /* Apparently, the trees could be located anywhere in the buffer
              * even if there's only 1 tree in the buffer. I'm assuming this is
@@ -198,37 +219,35 @@ namespace TreeAnarchy
             {
                 if (buffer[i].m_flags != 0)
                 {
-                    if (PreviousSaveWasOldFormat) buffer[i].m_posY = UpdatePosY(buffer[i].Position);
-                    WriteUShort(s, buffer[i].m_posY);
+                    WriteUShort(buffer[i].m_posY);
                     orgTreeCount++;
                 }
             }
             for(uint i = DefaultTreeLimit; i < treeLimit; i++)
             {
-                WriteUShort(s, buffer[i].m_flags);
+                WriteUShort(buffer[i].m_flags);
             }
             for(uint i = DefaultTreeLimit; i < treeLimit; i++)
             {
                 if (buffer[i].m_flags != 0)
                 {
-                    WriteUShort(s, buffer[i].m_infoIndex);
-                    WriteShort(s, buffer[i].m_posX);
-                    WriteShort(s, buffer[i].m_posZ);
-                    if (PreviousSaveWasOldFormat) buffer[i].m_posY = UpdatePosY(buffer[i].Position);
-                    WriteUShort(s, buffer[i].m_posY);
+                    WriteUShort(buffer[i].m_infoIndex);
+                    WriteShort(buffer[i].m_posX);
+                    WriteShort(buffer[i].m_posZ);
+                    WriteUShort(buffer[i].m_posY);
                     extraTreeCount++;
                 }
             }
             // Set header information now
-            s.Position = 6;
-            s.WriteByte((byte)extraTreeCount);
-            s.WriteByte((byte)(extraTreeCount >> 8));
-            s.WriteByte((byte)(extraTreeCount >> 16));
-            s.WriteByte((byte)(extraTreeCount >> 24));
-            s.WriteByte((byte)(orgTreeCount));
-            s.WriteByte((byte)(orgTreeCount >> 8));
-            s.WriteByte((byte)(orgTreeCount >> 16));
-            s.WriteByte((byte)(orgTreeCount >> 24));
+            m_Stream.Position = 6;
+            m_Stream.WriteByte((byte)extraTreeCount);
+            m_Stream.WriteByte((byte)(extraTreeCount >> 8));
+            m_Stream.WriteByte((byte)(extraTreeCount >> 16));
+            m_Stream.WriteByte((byte)(extraTreeCount >> 24));
+            m_Stream.WriteByte((byte)(orgTreeCount));
+            m_Stream.WriteByte((byte)(orgTreeCount >> 8));
+            m_Stream.WriteByte((byte)(orgTreeCount >> 16));
+            m_Stream.WriteByte((byte)(orgTreeCount >> 24));
 
 #if FALSE // Burning tree handled in prefix method
             // Let original codes handle the extra burning tree list. Hope it works
@@ -240,6 +259,7 @@ namespace TreeAnarchy
                 WriteUShort(s, instance.m_burningTrees.m_buffer[m].m_fireDamage);
             }
 #endif
+            return m_Stream.ToArray();
         }
 
         private int FixEndian(int num)
@@ -248,53 +268,53 @@ namespace TreeAnarchy
             return n | ((num >> 16) & 0xffff);
         }
 
-        private short ReadShort(Stream s)
+        private short ReadShort()
         {
-            short num = (short)s.ReadByte();
-            return (short)(num |= (short)(s.ReadByte() << 8));
+            short num = (short)m_Stream.ReadByte();
+            return (short)(num |= (short)(m_Stream.ReadByte() << 8));
 
         }
 
-        private int ReadInt32(Stream s)
+        private int ReadInt32()
         {
-            int num = s.ReadByte();
-            num |= s.ReadByte() << 8;
-            num |= s.ReadByte() << 16;
-            return num | s.ReadByte() << 24;
+            int num = m_Stream.ReadByte();
+            num |= m_Stream.ReadByte() << 8;
+            num |= m_Stream.ReadByte() << 16;
+            return num | m_Stream.ReadByte() << 24;
         }
 
-        private ushort ReadUShort(Stream s)
+        private ushort ReadUShort()
         {
-            ushort num = (ushort)(s.ReadByte() & 0xff);
-            return (ushort)(num | (s.ReadByte() << 8));
+            ushort num = (ushort)(m_Stream.ReadByte() & 0xff);
+            return (ushort)(num | (m_Stream.ReadByte() << 8));
         }
 
-        public void WriteShort(Stream s, short value)
+        public void WriteShort(short value)
         {
-            s.WriteByte((byte)(value));
-            s.WriteByte((byte)(value >> 8));
+            m_Stream.WriteByte((byte)(value));
+            m_Stream.WriteByte((byte)(value >> 8));
         }
 
-        private void WriteInt32(Stream s, int value)
+        private void WriteInt32(int value)
         {
-            s.WriteByte((byte)value);
-            s.WriteByte((byte)(value >> 8));
-            s.WriteByte((byte)(value >> 16));
-            s.WriteByte((byte)(value >> 24));
+            m_Stream.WriteByte((byte)value);
+            m_Stream.WriteByte((byte)(value >> 8));
+            m_Stream.WriteByte((byte)(value >> 16));
+            m_Stream.WriteByte((byte)(value >> 24));
         }
 
-        private void WriteUShort(Stream s, ushort value)
+        private void WriteUShort(ushort value)
         {
-            s.WriteByte((byte)value);
-            s.WriteByte((byte)(value >> 8));
+            m_Stream.WriteByte((byte)value);
+            m_Stream.WriteByte((byte)(value >> 8));
         }
 
-        private void WriteUInt32(Stream s, uint value)
+        private void WriteUInt32(uint value)
         {
-            s.WriteByte((byte)value);
-            s.WriteByte((byte)(value >> 8));
-            s.WriteByte((byte)(value >> 16));
-            s.WriteByte((byte)(value >> 24));
+            m_Stream.WriteByte((byte)value);
+            m_Stream.WriteByte((byte)(value >> 8));
+            m_Stream.WriteByte((byte)(value >> 16));
+            m_Stream.WriteByte((byte)(value >> 24));
         }
 
         public void Dispose()
@@ -309,6 +329,7 @@ namespace TreeAnarchy
             {
                 if (disposing)
                 {
+                    m_Stream.Dispose();
                     if (m_tempBuf != null) m_tempBuf.Dispose();
                 }
                 disposed = true;
