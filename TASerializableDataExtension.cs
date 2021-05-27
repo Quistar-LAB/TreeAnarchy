36ada055569f6c935b79329b9b43ba3e363eeccc
Simon Ueng <simon.ueng@outlook.com>
2021-05-28 06:14:08 +0800

    Consider old format not saving posY

--- TASerializableDataExtension.cs
+++ TASerializableDataExtension.cs
@@ -13,7 +13,16 @@ namespace TreeAnarchy
         static private ISerializableData m_Serializer = null;
         private class DataSerializer : TADataSerializer
         {
-            // Used for saving trees indexed > 262144
+            public DataSerializer() : base()
+            {
+            }
+            public DataSerializer(int size) : base(size)
+            {
+            }
+            public DataSerializer(byte[] data) : base(data)
+            {
+            }
+
             public override void FinalizeSave()
             {
             }
@@ -34,19 +43,29 @@ namespace TreeAnarchy
 
         public void OnSaveData()
         {
-            const int headerSize = 100; // over estimate header size
-            const int eachTreeByteCount = 10;
-
             PurgeData(); // remove old data if it exists
+            if (OldFormatLoaded)
+            {
+                /* When using original CO or Unlimited Trees Mod, the posY is never
+                 * considered, and it could be any random number, usually 0. When
+                 * saving into our new format. We need to actually store their posY
+                 * so we have to make sure its initialized.
+                 */
+                TreeInstance[] trees = Singleton<TreeManager>.instance.m_trees.m_buffer;
+                for(uint i = 1; i < MaxTreeLimit; i++)
+                {
+                    TreeManager.instance.m_trees.m_buffer[i].CalculateTree(i);
+                }
+                OldFormatLoaded = false;
+            }
             try
             {
                 // hmmmm assuming 1 million trees... this is around ~10mb of data
                 // lets see if compression helps improve File IO
+                // Use dynamic memory allocation, to possibly save ram during saving
                 using (DataSerializer serializer = new DataSerializer())
-                using (MemoryStream stream = new MemoryStream((MaxTreeLimit * eachTreeByteCount) + headerSize))
                 {
-                    serializer.Serialize(stream);
-                    m_Serializer?.SaveData(OldTreeUnlimiterKey, stream.ToArray());
+                    m_Serializer?.SaveData(OldTreeUnlimiterKey, serializer.Serialize());
                 }
             }
             catch (Exception e)
@@ -67,10 +86,15 @@ namespace TreeAnarchy
                     Debug.Log("TreeAnarchy: Invalid Data");
                     return;
                 }
-                using (DataSerializer serializer = new DataSerializer())
-                using (MemoryStream stream = new MemoryStream(data))
+                using (DataSerializer serializer = new DataSerializer(data))
                 {
-                    serializer.Deserialize(stream);
+                    TADataSerializer.ErrorFlags errors;
+                    errors = serializer.Deserialize(Singleton<TreeManager>.instance.m_trees.m_buffer);
+                    if ((errors & TADataSerializer.ErrorFlags.OLDFORMAT) != TADataSerializer.ErrorFlags.NONE)
+                    {
+                        Debug.Log("TreeAnarchy: Old Format Detected");
+                        OldFormatLoaded = true;
+                    }
                 }
                 return;
             }
