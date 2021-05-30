4affbd434b3eba7aceca7224170c5124b0202d1c
Simon Ueng <simon.ueng@outlook.com>
2021-05-31 07:10:42 +0800

    new class for a custom slider

---
+++ UI/CustomSlider.cs
@@ -0,0 +1,23 @@
+﻿using ColossalFramework;
+using ColossalFramework.UI;
+
+namespace TreeAnarchy.UI
+{
+    class CustomSlider : UISlider
+    {
+        public override void OnDisable()
+        {
+            this.eventClick -= CustomSlider_eventClick;
+        }
+
+        public override void OnEnable()
+        {
+            this.eventClick += CustomSlider_eventClick;
+        }
+
+        private void CustomSlider_eventClick(UIComponent component, UIMouseEventParameter eventParam)
+        {
+            
+        }
+    }
+}
