d542c22aaa7c44d1689f5c337f3a5d8c9c9ad9fa
Quistar <simon.ueng@outlook.com>
2021-07-13 03:39:44 +0800

    Version bump

--- TAMod.cs
+++ TAMod.cs
@@ -7,7 +7,7 @@ using System.Xml;
 
 namespace TreeAnarchy {
     public class TAMod : ILoadingExtension, IUserMod {
-        internal const string m_modVersion = "0.9.4";
+        internal const string m_modVersion = "0.9.5";
         internal const string m_assemblyVersion = m_modVersion + ".*";
         private const string m_modName = "Unlimited Trees: Reboot";
         private const string m_modDesc = "An improved Unlimited Trees Mod. Lets you plant more trees with tree snapping";
