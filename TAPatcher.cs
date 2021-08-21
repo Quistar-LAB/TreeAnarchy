a2b0c152e6c492ac85fc3770dfe71185802e4bd6
Quistar <simon.ueng@outlook.com>
2021-08-21 23:21:24 +0800

    Patcher re-organization

--- TAPatcher.cs
+++ TAPatcher.cs
@@ -1,21 +1,49 @@
 ﻿using ColossalFramework;
+using ColossalFramework.PlatformServices;
 using ColossalFramework.Plugins;
+using ColossalFramework.UI;
 using HarmonyLib;
+using System;
 using System.Reflection;
-using TreeAnarchy.Patches;
 
 namespace TreeAnarchy {
-    internal class TAPatcher {
-        internal const string HARMONYID = @"quistar.treeanarchy.mod";
-
-        private static bool isCorePatched = false;
+    internal partial class TAPatcher : SingletonLite<TAPatcher> {
+        private const string HARMONYID = @"quistar.treeanarchy.mod";
+        private Harmony m_harmony;
+        private Harmony CurrentHarmony {
+            get => (m_harmony is null) ? m_harmony = new Harmony(HARMONYID) : m_harmony;
+        }
         internal static FieldInfo MoveItUseTreeSnap = null;
-        internal static bool isMoveItBeta = false;
+        internal static bool isMoveItInstalled = false;
+        private struct ModInfo {
+            public readonly ulong fileID;
+            public readonly string name;
+            public ModInfo(ulong modID, string modName) {
+                fileID = modID;
+                name = modName;
+            }
+        }
+        private static readonly ModInfo[] IncompatibleMods = new ModInfo[] {
+            new ModInfo(455403039, "Unlimited Trees Mod"),
+            new ModInfo(2378914031, "Unlimited Trees Revisited"),
+            new ModInfo(869134690, "Tree Snapping"),
+            new ModInfo(1637106958, "Lock Forestry"),
+            new ModInfo(556784825, "Random Tree Rotation"),
+            new ModInfo(1388613752, "Tree Movement Control"),
+            new ModInfo(842981708, "Random Tree Rotation for Natural Disasters")
+        };
 
-        private bool IsPluginExists(string id, string name) {
+        internal static bool IsPluginExists(ulong id, string name) {
             foreach (PluginManager.PluginInfo info in Singleton<PluginManager>.instance.GetPluginsInfo()) {
-                if (info.name.Contains(id) || info.ToString().Contains(name)) {
+                if (info.publishedFileID.AsUInt64 == id || info.ToString().Contains(name)) {
                     if (info.isEnabled) return true;
+                }
+            }
+            foreach (var mod in PlatformService.workshop.GetSubscribedItems()) {
+                for (int i = 0; i < IncompatibleMods.Length; i++) {
+                    if (mod.AsUInt64 == id) {
+                        return true;
+                    }
                 }
             }
             return false;
@@ -26,47 +54,68 @@ namespace TreeAnarchy {
             if (treeSnapField != null) {
                 MoveItUseTreeSnap = treeSnapField;
                 MoveItUseTreeSnap.SetValue(null, TAMod.UseTreeSnapping);
-                isMoveItBeta = true;
+                isMoveItInstalled = true;
                 return true;
             }
             return false;
         }
 
-        internal void EnableCore() {
-            Harmony harmony = new Harmony(HARMONYID);
-            if (!isCorePatched) {
-                TreeLimit treeLimit = new TreeLimit();
-                treeLimit.Enable(harmony);
-                TreeSnapping treeSnapping = new TreeSnapping();
-                treeSnapping.Enable(harmony);
-                TreeVariation treeVariation = new TreeVariation();
-                treeVariation.EnablePatch(harmony);
-                isCorePatched = true;
+        internal bool CheckIncompatibleMods() {
+            string errorMsg = "";
+            foreach (var mod in PlatformService.workshop.GetSubscribedItems()) {
+                for (int i = 0; i < IncompatibleMods.Length; i++) {
+                    if (mod.AsUInt64 == IncompatibleMods[i].fileID) {
+                        errorMsg += $"[{IncompatibleMods[i].name}] detected\n";
+                        TAMod.TALog($"Incompatible mod: [{IncompatibleMods[i].name}] detected");
+                    }
+                }
             }
+            if (errorMsg.Length > 0) {
+                UIView.ForwardException(new Exception("Tree Anarchy detected incompatible mods, please remove the following mentioned mods", new Exception("\n" + errorMsg)));
+                TAMod.TALog($"Tree Anarchy detected incompatible mods, please remove the following mentioned mods\n{errorMsg}");
+                return false;
+            }
+            return true;
+        }
+
+        internal void EnableCore() {
+            Harmony harmony = CurrentHarmony;
+            EnableTreeLimitPatches(harmony);
+            EnableTreeSnappingPatches(harmony);
+            EnableTreeVariationPatches(harmony);
+#if ENABLETREEANARCHY
+            EnableTreeAnarchyPatches(harmony);
+#endif
+        }
+
+        internal void DisableCore() {
+            Harmony harmony = CurrentHarmony;
+            DisableTreeLimitPatches(harmony);
+            DisableTreeSnappingPatches(harmony);
+            DisableTreeVariationPatches(harmony);
+#if ENABLETREEANARCHY
+            DisableTreeAnarchyPatches(harmony);
+#endif
         }
 
         internal void LateEnable() {
-            Harmony harmony = new Harmony(HARMONYID);
-            TreeVariation treeVariation = new TreeVariation();
-            TreeMovement treeMovement = new TreeMovement();
-            treeMovement.Enable(harmony);
-            treeVariation.EnablePatch(harmony);
-
-            if (IsPluginExists("1619685021", "MoveIt") || IsPluginExists("2215771668", "MoveIt")) {
+            Harmony harmony = CurrentHarmony;
+            EnableTreeMovementPatches(harmony);
+            if (IsPluginExists(1619685021, "MoveIt") || IsPluginExists(2215771668, "MoveIt")) {
                 if (!CheckMoveItTreeSnapSig()) {
-                    TreeSnapping treeSnapping = new TreeSnapping();
-                    treeSnapping.PatchMoveIt(harmony);
+                    PatchMoveItSnapping(harmony);
                 }
-                treeVariation.PatchMoveIt(harmony);
+                PatchMoveItTreeVariation(harmony);
             }
         }
 
         internal void DisableLatePatch() {
-            Harmony harmony = new Harmony(HARMONYID);
-            TreeVariation treeVariation = new TreeVariation();
-            treeVariation.DisablePatch(harmony);
+            Harmony harmony = CurrentHarmony;
+            DisableTreeMovementPatches(harmony);
+            if (isMoveItInstalled) {
+                DisableMoveItSnappingPatches(harmony);
+            }
+            DisableMoveItTreeVariationPatches(harmony);
         }
-
-        internal void DisableCore() { }
     }
 }
