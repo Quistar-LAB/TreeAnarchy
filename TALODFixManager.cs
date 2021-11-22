f2608781fe403e896be22f44524645d25d35f959
Quistar <simon.ueng@outlook.com>
2021-11-23 07:41:41 +0800

    Include Tree LOD Fix framework

---
+++ TALODFixManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace TreeAnarchy {
+    public static partial class TAManager {
+        private const int LOD_LODRES = 512;
+        private const int LOD_MEDIUMRES = 1024;
+        private const int LOD_HIGHRES = 2048;
+        private const int LOD_ULTRARES = 4096;
+        public enum TreeLODResolution : int {
+            Low,
+            Medium,
+            High,
+            UltraHigh
+        }
+
+        public static void SetResolution(this TreeManager manager, TreeLODResolution resolution) {
+            int res = LOD_LODRES;
+            switch (resolution) {
+            //case TreeLODResolution.Low: res = LOD_LODRES; break;
+            case TreeLODResolution.Medium: res = LOD_MEDIUMRES; break;
+            case TreeLODResolution.High: res = LOD_HIGHRES; break;
+            case TreeLODResolution.UltraHigh: res = LOD_ULTRARES; break;
+            }
+            manager.m_renderDiffuseTexture = new RenderTexture(res, res, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear) {
+                filterMode = FilterMode.Trilinear,
+                autoGenerateMips = true
+            };
+            manager.m_renderXycaTexture = new RenderTexture(res, res, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear) {
+                filterMode = FilterMode.Trilinear,
+                autoGenerateMips = true
+            };
+            manager.m_renderDiffuseTexture.autoGenerateMips = true;
+            manager.m_renderShadowTexture = new RenderTexture(res, res, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear) {
+                filterMode = FilterMode.Point,
+                autoGenerateMips = false
+            };
+        }
+    }
+}
