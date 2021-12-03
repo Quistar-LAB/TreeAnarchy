8a68afef36b2fcb2e28c9236291e73e84f74c402
Quistar <simon.ueng@outlook.com>
2021-12-03 14:29:38 +0800

    Add multiple checks to directory and filenames to cope with LOM

--- TALocale.cs
+++ TALocale.cs
@@ -3,12 +3,15 @@ using ColossalFramework.Globalization;
 using ColossalFramework.PlatformServices;
 using System;
 using System.Globalization;
+using System.IO;
 using System.Xml;
+using TreeAnarchy.Localization;
 
 namespace TreeAnarchy {
     internal static class TALocale {
         private const ulong m_thisModID = 2527486462;
         private const ulong m_betaModID = 2584051448;
+        private const string DefaultEnLocale = @"TreeAnarchy.en.locale";
         private static XmlDocument m_xmlLocale;
         private static string m_directory;
         private static bool isInitialized = false;
@@ -45,16 +48,20 @@ namespace TreeAnarchy {
         }
 
         private static void LoadLocale(string culture) {
-            string localeFile = m_directory + @"TreeAnarchy." + culture + @".locale";
-            XmlDocument locale = new XmlDocument();
+            XmlDocument locale = new XmlDocument {
+                XmlResolver = null
+            };
             try {
+                string localeFile = m_directory + @"TreeAnarchy." + culture + @".locale";
                 locale.Load(localeFile);
             } catch {
-                /* Load default english locale */
-                localeFile = m_directory + @"TreeAnarchy.en.locale";
-                locale.Load(localeFile);
+                /* Load default english locale embedded in assembly */
+                using (MemoryStream s = new MemoryStream(DefaultLocale.TreeAnarchy_en)) {
+                    locale.Load(s);
+                }
+            } finally {
+                m_xmlLocale = locale;
             }
-            m_xmlLocale = locale;
         }
 
         internal static void Init() {
@@ -62,16 +69,25 @@ namespace TreeAnarchy {
                 try {
                     foreach (PublishedFileId fileID in PlatformService.workshop.GetSubscribedItems()) {
                         if (fileID.AsUInt64 == m_thisModID) {
-                            m_directory = PlatformService.workshop.GetSubscribedItemPath(fileID) + @"/Locale/";
-                            break;
+                            string dir = PlatformService.workshop.GetSubscribedItemPath(fileID) + @"/Locale/";
+                            if (Directory.Exists(dir) && File.Exists(dir + DefaultEnLocale)) {
+                                m_directory = dir;
+                                break;
+                            }
                         }
                         if (fileID.AsUInt64 == m_betaModID) {
-                            m_directory = PlatformService.workshop.GetSubscribedItemPath(fileID) + @"/Locale/";
-                            break;
+                            string dir = PlatformService.workshop.GetSubscribedItemPath(fileID) + @"/Locale/";
+                            if (Directory.Exists(dir) && File.Exists(dir + DefaultEnLocale)) {
+                                m_directory = dir;
+                                break;
+                            }
                         }
                     }
                     if (m_directory is null) {
-                        m_directory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"/Colossal Order/Cities_Skylines/Addons/Mods/TreeAnarchy/Locale/";
+                        string dir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"/Colossal Order/Cities_Skylines/Addons/Mods/TreeAnarchy/Locale/";
+                        if (Directory.Exists(dir) && File.Exists(dir + DefaultEnLocale)) {
+                            m_directory = dir;
+                        }
                     }
                 } catch (Exception e) {
                     UnityEngine.Debug.LogException(e);
